5c52160fde9ae62075c6e5ceec35dcf05d82c7a3
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-04 13:45:28 +0300

    API ve UI katmanı eklendi.API katmanı entitiler eklendi.Bir sonraki adımda katmanlı mimariye geçiş yapılacak.

---
+++ SignalRApi/DAL/Entity/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRApi.DAL.Entity
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalRApi/DAL/Entity/Discount.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRApi.DAL.Entity
+{
+    public class Discount
+    {
+        public int MyProperty { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
